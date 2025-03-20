9ef82153572077733fc39f4253a154915dc36f32
Yana <yana.samusenko@internet.ru>
2025-03-20 05:14:57 +0300

    Updates

--- webReact/src/components/Button.jsx
+++
@@ -1,8 +0,0 @@
-import '../styles/Button.scss'
-
-export function Button ({type = 'button', name, children}) {
-    const className = `button ${name ? 'button_' + name : ''}`
-    return (
-        <button className={className} type={type}>{children}</button>
-    )
-}
--- webReact/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import { App } from './App.jsx'
-const rootElement = document.querySelector('#root')
-const root = createRoot(rootElement)
-
-root.render(
-  <App />,
-)
